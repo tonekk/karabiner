dd8dfc8e8295f329a4b96a84573eac5f196426d5
Finn Heemeyer <finn.heemeyer@comgy.io>
2023-02-20 13:52:54 +0100

    Add rules to make built-in keyboard behave like moonlander
    I'm going to regret this...

--- rules.ts
+++ rules.ts
@@ -3,11 +3,849 @@ import { KarabinerRules } from "./types";
 import { createHyperSubLayers, app, open } from "./utils";
 
 const rules: KarabinerRules[] = [
-  // Define the Hyper key itself
   {
-    description: "Hyper Key (⌃⌥⇧⌘)",
+    description: "Make interal keyboard behave like moonlander",
     manipulators: [
-      { type: "basic",
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "spacebar",
+        },
+        to: [
+          {
+            key_code: "right_command",
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "fn",
+        },
+        to: [
+          {
+            key_code: "left_command",
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "left_command",
+        },
+        to: [
+          {
+            key_code: "spacebar",
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "d",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "9",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "f",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "0",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "c",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "open_bracket",
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "v",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "close_bracket",
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "e",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "open_bracket",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "r",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "close_bracket",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "q",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "quote",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "w",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "quote",
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "m",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "hyphen",
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "j",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "hyphen",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "i",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "backslash",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "h",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "3",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "g",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "equal_sign",
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "b",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "1",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "p",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "5",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "k",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "7",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "l",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "2",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "n",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "equal_sign",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "t",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "4",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "y",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "backslash",
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "x",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "8",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "z",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "6",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "comma",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "comma",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "period",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "period",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "slash",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "slash",
+            modifiers: ["shift"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "a",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "a",
+            modifiers: ["left_option"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "o",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "o",
+            modifiers: ["left_option"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "u",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "u",
+            modifiers: ["left_option"],
+          },
+        ],
+      },
+      {
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+        from: {
+          key_code: "s",
+          modifiers: {
+            mandatory: ["right_command"],
+          },
+        },
+        to: [
+          {
+            key_code: "s",
+            modifiers: ["left_option"],
+          },
+        ],
+      },
+    ]
+  },
+  {
+    description: "Hyper & Meh Key",
+    manipulators: [
+      {
+        description: "Backslash -> Hyper Key",
+        from: {
+          key_code: "backslash",
+        },
+        to: [
+          {
+            key_code: "left_shift",
+            modifiers: ["left_command", "left_control", "left_option"],
+          },
+        ],
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+      },
+      {
+        description: "Right command -> Meh Key",
+        from: {
+          key_code: "right_command",
+        },
+        to: [
+          {
+            key_code: "left_shift",
+            modifiers: ["left_control", "left_option"],
+          },
+        ],
+        type: "basic",
+        conditions: [
+          {
+            type: "device_if",
+            identifiers: [
+              {
+                is_built_in_keyboard: true,
+              }
+            ]
+          }
+        ],
+      },
+    ],
+  },
+  {
+    description: "Hyper Key Apps",
+    manipulators: [
+      { 
+        type: "basic",
         description: "Disable CMD + Tab to force Hyper Key usage",
         from: {
           key_code: "tab",
@@ -39,9 +877,9 @@ const rules: KarabinerRules[] = [
       },
       {
         type: "basic",
-        description: "Open alacritty",
+        description: "Open terminal",
         from: {
-          key_code: "a",
+          key_code: "t",
           modifiers: {
             mandatory: [
               "left_command",
@@ -55,9 +893,9 @@ const rules: KarabinerRules[] = [
       },
       {
         type: "basic",
-        description: "Open telegram",
+        description: "Open teleGram",
         from: {
-          key_code: "t",
+          key_code: "g",
           modifiers: {
             mandatory: [
               "left_command",
@@ -71,9 +909,9 @@ const rules: KarabinerRules[] = [
       },
       {
         type: "basic",
-        description: "Open Chrome",
+        description: "Open browser",
         from: {
-          key_code: "c",
+          key_code: "b",
           modifiers: {
             mandatory: [
               "left_command",
